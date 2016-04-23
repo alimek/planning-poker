2e895fedfde1b476f84e7b452faf6481f9554d6c
maczkus <maczek@maczek.net>
2016-04-23 16:05:08 +0200

    fixed postAction to return json

--- src/AppBundle/Controller/GamesController.php
+++ src/AppBundle/Controller/GamesController.php
@@ -7,7 +7,7 @@ use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Routing\ClassResourceInterface;
 use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
 
 class GamesController extends FOSRestController implements ClassResourceInterface
 {
@@ -15,19 +15,21 @@ class GamesController extends FOSRestController implements ClassResourceInterfac
     {
     }
 
+    /**
+     * @Rest\View()
+     */
     public function postAction()
     {
         $game = new Game();
+        $game->setHash('asdasas');
         $dm = $this->get('doctrine_mongodb')->getManager();
         $dm->persist($game);
         $dm->flush();
 
-        $view = View::create();
+        $view = $this->view($game);
+        $view->setFormat('json');
 
-//        $view->setFormat();
-
-        $view->setData($game);
-        return $view->get();
+        return $this->handleView($view);
     }
 
     public function getAction($gameId)
