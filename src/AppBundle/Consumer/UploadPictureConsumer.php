26cddc018d7af795eb3861c13b396dd00dabdc07
Rafał Treffler <rafal.treffler+github@gmail.com>
2016-06-04 12:06:53 +0200

    clears repo

--- src/AppBundle/Consumer/UploadPictureConsumer.php
+++ src/AppBundle/Consumer/UploadPictureConsumer.php
@@ -13,13 +13,13 @@ class UploadPictureConsumer implements ConsumerInterface
         //$msg will be an instance of `PhpAmqpLib\Message\AMQPMessage` with the $msg->body being the data sent over RabbitMQ.
 
 //        $isUploadSuccess = someUploadPictureMethod();
-        if (!$isUploadSuccess) {
+//        if (!$isUploadSuccess) {
             // If your image upload failed due to a temporary error you can return false
             // from your callback so the message will be rejected by the consumer and
             // requeued by RabbitMQ.
             // Any other value not equal to false will acknowledge the message and remove it
             // from the queue
-            return false;
-        }
+//            return false;
+//        }
     }
 }
--- src/AppBundle/Util/SingularizeInflector.php
+++ src/AppBundle/Util/SingularizeInflector.php
@@ -6,6 +6,10 @@ use FOS\RestBundle\Util\Inflector\InflectorInterface;
 
 class SingularizeInflector implements InflectorInterface
 {
+    /**
+     * @param string $word
+     * @return string
+     */
     public function pluralize($word)
     {
         return $word;
