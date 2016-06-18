f617c695a398ff45414ad79560104ecaa80e3f85
Krzysztof Maczkowiak <maczek@maczek.net>
2016-06-18 16:12:40 +0200

    modified TasksController routes; embedding tasks

--- src/AppBundle/Repositories/GameRepository.php
+++ src/AppBundle/Repositories/GameRepository.php
@@ -7,9 +7,19 @@ use Doctrine\ODM\MongoDB\DocumentRepository;
 
 class GameRepository extends DocumentRepository
 {
+    /**
+     * @param Game $game
+     */
     public function save(Game $game)
     {
         $this->getDocumentManager()->persist($game);
         $this->getDocumentManager()->flush();
     }
+
+//    public function getTask($taskId)
+//    {
+//        $this->createQueryBuilder()
+//            ->field('task')
+//    }
+
 }
